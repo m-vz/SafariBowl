31db401ecf52648b8d57632ae8f3608f7f823a4d
Giovanni <Giovanni@Eddie>
2015-06-22 13:50:53 +0200

    SafariBowl 0.9.1

--- src/gameLogic/dice/BlockDie.java
+++ src/gameLogic/dice/BlockDie.java
@@ -8,23 +8,14 @@ import java.awt.image.BufferedImage;
  * The die thrown while blocking
  */
 public class BlockDie implements Die {
-	private int throwCounter = 0;
 	/**
 	 * @return	Returns a int from 1 to 5 where the numbers mean 1: "Attacker Down", 2: "Both Down", 3: "Pushed", 4: "Defender Stumbles", 5: "Defender Down"
 	 */
 	public int throwDie() {
-		/*int[] throwsForPresentation = {5, 3, 3, 1, 3, 5, 1, 3, 5, 4, 3, 2, 3, 4, 1, 1, 3, 3, 3, 5, 3, 4, 5, 2, 4, 4};
-		if(throwCounter < throwsForPresentation.length){
-			int i = throwsForPresentation[throwCounter];
-			throwCounter++;
-			System.out.println("DB: " + i);
-			return i;
-		}*/
 		int i = rand.nextInt(6) + 1;
 		if(i == 6){
 			i = 3;
 		}
-//		System.out.println("DB: " + i);
 		return i;
 	}
 
